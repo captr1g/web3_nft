d2230af47f9d6ef74b2fc280c84f9a723d468ce5
Yash Raj Saxena <yashrajsaxena1@gmail.com>
2024-01-19 00:19:39 +0530

    fix deployment error

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import F5 from './images/fl5.jpg';
 import F6 from './images/fl6.jpg';
 import BackgroundAni from './components/backgroun';
 import NavBar from './components/navBar';
-import getWeb3 from 'flower-shop/src/web3.js';
+import getWeb3 from './web3';
 
 
 
